5fcd2a3a8f3aa487d9998d8b79b3857189feddb2
leedMx <esparza.mexico@gmail.com>
2021-02-22 07:58:25 -0600

    Debugging customer without List<Pet>

--- src/main/java/com/udacity/jdnd/course3/critter/entity/Customer.java
+++ src/main/java/com/udacity/jdnd/course3/critter/entity/Customer.java
@@ -15,6 +15,6 @@ public class Customer {
     private String name;
     private String phoneNumber;
     private String notes;
-    @OneToMany (mappedBy = "customerId")
+    @OneToMany (mappedBy = "customerId", fetch = FetchType.LAZY)
     private List<Pet> pets = new LinkedList<>();
 }
--- src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
@@ -35,6 +35,9 @@ public class CustomerService {
     }
 
     public List<Customer> getAllCustomers() {
-        return new LinkedList<>(customerRepository.findAll());
+        System.out.println("=========== Finding customers ===========");
+        List<Customer> found = customerRepository.findAll();
+        System.out.println(found);
+        return new LinkedList<>(found);
     }
 }
